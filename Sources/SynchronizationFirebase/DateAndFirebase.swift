c4b814c4b8a79ba1d7fed461a69ee26e58e93256
Thomas Hirsch <thirsch@smartadserver.com>
2023-10-26 10:23:22 +0200

    Use fixed Firebase safe date

--- Sources/SynchronizationFirebase/DateAndFirebase.swift
+++ Sources/SynchronizationFirebase/DateAndFirebase.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public extension Date {
     static var distantPastFirebaseSafe: Date {
-        Date().addingTimeInterval(-50 * 365 * 24 * 60 * 60)
+        Date(timeIntervalSince1970: TimeInterval(0))
     }
 }
 
